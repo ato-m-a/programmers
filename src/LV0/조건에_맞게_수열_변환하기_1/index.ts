6ecb2082dad0b133311e350e3ca7f05ae4c27364
Tom <99224451+ato-m-a@users.noreply.github.com>
2023-11-24 00:48:27 +0900

    조건에 맞게 수열 변환하기 1 완료

---
+++ src/LV0/조건에_맞게_수열_변환하기_1/index.ts
@@ -0,0 +1,7 @@
+export const solution = (arr: number[]) => {
+  return arr.map(v => {
+    if (v >= 50 && v % 2 === 0) return v / 2;
+    if (v < 50 && v % 2 !== 0) return v * 2;
+    return v;
+  });
+}
