d951293c9e5bc2e0cc61ba3d3a25f793ef2c6aaf
Tom <99224451+ato-m-a@users.noreply.github.com>
2023-11-24 01:30:22 +0900

    1로 만들기 완료

---
+++ src/LV0/1로_만들기/index.ts
@@ -0,0 +1,13 @@
+export const solution = (num_list: number[]) => {
+  let answer = 0;
+
+  for (let num of num_list) {
+    while (num > 1) {
+      if (num % 2 === 0) num /= 2;
+      else num = (num - 1) / 2;
+      answer++;
+    }
+  }
+
+  return answer;
+}
